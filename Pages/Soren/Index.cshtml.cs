a27512e9a4d55c3e1cb4e08c58249f08f1226edc
Jacob-Knugget <ande1186@mail.nmc.edu>
2024-09-22 17:48:35 -0400

    Add project files.

---
+++ Pages/Soren/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RestaurantReviewer.Data;
+using RestaurantReviewer.Models;
+
+namespace RestaurantReviewer.Pages.Soren
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RestaurantReviewer.Data.RestaurantReviewerContext _context;
+
+        public IndexModel(RestaurantReviewer.Data.RestaurantReviewerContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Restaurants> Restaurants { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Restaurants != null)
+            {
+                Restaurants = await _context.Restaurants.ToListAsync();
+            }
+        }
+    }
+}

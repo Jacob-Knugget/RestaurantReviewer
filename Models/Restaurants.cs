881fc40f5d24e9f1c23052ef7a871beb64e2f070
Jacob-Knugget <ande1186@mail.nmc.edu>
2024-10-04 00:52:35 -0400

    fixed

--- Models/Restaurants.cs
+++ Models/Restaurants.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RestaurantReviewer.Models
 {
     public class Restaurants
     {
-        public string? ImageData { get; set; }
         public int ID { get; set; }
+        public byte[]? RestaurantPicture { get; set; }
         public string? Name { get; set; }
         public double? Rating { get; set; }
         public string? Type { get; set; }
@@ -13,8 +14,5 @@ namespace RestaurantReviewer.Models
         public string? Cost { get; set; }
         [NotMapped]
         public List<string>? Reviews { get; set; } = new List<string>();
-
-        [NotMapped]
-        public IFormFile? ImageFile { get; set; }
     }
 }
